ac89ac51ea2ea52855f3cb3a779e99ef4d4e59a2
p1asmic <plasmicbeam@gmail.com>
2025-05-14 20:15:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -88,5 +88,5 @@ def upload_audio():
     return jsonify(result)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
